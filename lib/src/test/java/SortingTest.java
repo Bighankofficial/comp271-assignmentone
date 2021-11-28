f6f52e21b1269d87b391e343b869a50a7c09f56d
Kieran Murphy <kmurphy37@luc.edu>
2021-11-28 17:58:34 -0600

    adds test for SelectionSort

--- lib/src/test/java/SortingTest.java
+++ lib/src/test/java/SortingTest.java
@@ -9,4 +9,23 @@ public class SortingTest {
         Integer min = mergeSort.removeMin();
         assertEquals(1, min);
     }
+
+    @Test
+    public void runSelectionSort() {
+
+        // given
+        SelectionSort selectionSort = new SelectionSort();
+        int[] list = {3, 2 ,0, 4, 5, 1};
+
+        // when
+        int[] sortedList = selectionSort.selectionSort(list);
+
+        // then
+        assertEquals(0, sortedList[0]);
+        assertEquals(1, sortedList[1]);
+        assertEquals(2, sortedList[2]);
+        assertEquals(3, sortedList[3]);
+        assertEquals(4, sortedList[4]);
+        assertEquals(5, sortedList[5]);
+    }
 }
