dc4d4bc10ba1e0e75c50c7a3e93ef2803e1878d0
Henry Nichols <henryk.nichols@gmail.com>
2021-11-28 17:32:53 -0600

    SortingAlgos

--- lib/src/main/java/MergeSort.java
+++ lib/src/main/java/MergeSort.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MergeSort {
     private final List<Integer> integers;
     public MergeSort(Integer... integers) {
--- lib/src/test/java/SortingTest.java
+++ lib/src/test/java/SortingTest.java
@@ -4,8 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SortingTest {
     @Test
-    public void runMergeSort(){
-        MergeSort mergeSort = new MergeSort(1,4,6,7);
+    public void runMergeSort() {
+        MergeSort mergeSort = new MergeSort(1, 4, 6, 7);
         Integer min = mergeSort.removeMin();
-        assertEquals(1,min);
+        assertEquals(1, min);
+    }
 }
