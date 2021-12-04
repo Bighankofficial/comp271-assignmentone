d7a45f167ed28725cc932041fbba3ab483cf4f4e
Henry Nichols <henryk.nichols@gmail.com>
2021-12-03 21:33:27 -0600

    UpdatedSortingTest

--- lib/src/main/java/QuickSort.java
+++ lib/src/main/java/QuickSort.java
@@ -3,28 +3,72 @@ import java.util.List;
 
 public class QuickSort {
 
-    private final List<Integer> integers;
+    private int[] integers;
 
-    public QuickSort(Integer... integers) {
-        this.integers = new ArrayList<>();
-        for (Integer integer : integers) {
-            this.integers.add(integer);
+    public QuickSort(Integer... intsArg) {
+        integers = new int[intsArg.length];
+        for (int i = 0; i < intsArg.length; i++)
+        {
+            integers[i] = intsArg[i];
         }
+        QuickSortMain(integers, 0, integers.length - 1);
+
+    }
+    public void QuickSortMain(int[] intsArg, int left, int right)
+    {
+        if (left >= right)
+        {
+            return;
+        }
+        int leftPartition = partition(intsArg, left, right);
+        QuickSortMain(intsArg, left, leftPartition);
+        QuickSortMain(intsArg, leftPartition + 1, right);
     }
 
-    public static int partition(int[] array, int left, int right, int pivot) {
-        while(left <=right){
-            while (array[left]< pivot){
+    public int partition(int[] array, int left, int right) {
+        int midpoint = left + (right - left) / 2;
+        int pivot = array[midpoint];
+
+        boolean done = false;
+        while (!done)
+        {
+            while (array[left] < pivot)
+            {
                 left++;
             }
-            while(array[right]>pivot){
+            while (array[right] > pivot)
+            {
+                right--;
+            }
+
+            if (left >= right)
+            {
+                done = true;
+            }
+            else
+            {
+                int swap = array[left];
+                array[left] = array[right];
+                array[right] = swap;
+                left++;
                 right--;
             }
         }
-        return left;
+        return right;
     }
 
     public Integer removeMin() {
-        throw new UnsupportedOperationException("not implemented yet!");
+        if (integers == null || integers.length == 0)
+        {
+            return -1;
+        }
+        Integer retVal = integers[0];
+        int[] newArr = new int[integers.length - 1];
+        for (int i = 0; i < integers.length - 1; i++)
+        {
+            newArr[i] = integers[i+1];
+        }
+        integers = newArr;
+        return retVal;
     }
 }
